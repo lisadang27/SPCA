c87e8025be537bf74fedd1798de344e6eecafff7
Taylor James Bell <taylor.bell@mail.mcgill.ca>
2018-02-15 12:31:04 -0500

    Adding my versions of the files
    To be merged in collaboration with Lisa and made even more user friendly! :)

---
+++ astro_models.py
@@ -0,0 +1,145 @@
+import numpy as np
+import batman
+
+def transit_model(time, t0, per, rp, a, inc, ecc, w, u1, u2):
+    params = batman.TransitParams()                      #object to store transit parameters
+    params.t0 = t0                                       #time of inferior conjunction
+    params.per = per                                     #orbital period
+    params.rp = rp                                       #planet radius (in units of stellar radii)
+    params.a = a                                         #semi-major axis (in units of stellar radii)
+    params.inc = inc                                     #orbital inclination (in degrees)
+    params.ecc = ecc                                     #eccentricity
+    params.w = w                                         #longitude of periastron (in degrees)
+    params.limb_dark = "quadratic"                       #limb darkening model
+    params.u = [u1, u2]                                  #limb darkening coefficients
+
+    m = batman.TransitModel(params, time)                #initializes model
+    flux = m.light_curve(params)
+    t_secondary = m.get_t_secondary(params)
+    anom       = m.get_true_anomaly()
+    return flux, t_secondary, anom
+
+def eclipse(time, t0, per, rp, a, inc, ecc, w, u1, u2, fp, t_sec):
+    params = batman.TransitParams()                      #object to store transit parameters
+    params.t0 = t0                                       #time of inferior conjunction
+    params.per = per                                     #orbital period
+    params.rp = rp                                       #planet radius (in units of stellar radii)
+    params.a = a                                         #semi-major axis (in units of stellar radii)
+    params.inc = inc                                     #orbital inclination (in degrees)
+    params.ecc = ecc                                     #eccentricity
+    params.w = w                                         #longitude of periastron (in degrees)
+    params.limb_dark = "quadratic"                       #limb darkening model
+    params.u = [u1, u2]                                  #limb darkening coefficients
+    params.fp = fp                                       #planet/star brightnes
+    params.t_secondary = t_sec
+    
+    m = batman.TransitModel(params, time, transittype="secondary")  #initializes model
+    flux = m.light_curve(params)
+    return flux
+
+def area(time, t_sec, per, rp, inc_raw, p0_phase, mode):
+    #FOR NOW I AM ASSUMING ONLY 1st ORDER VARIATIONS PERMITTED WITH AREA VARIATIONS!!!
+    r2 = p0_phase[2]
+    
+    t = time - t_sec
+    w = 2*np.pi/per
+    phi = (w*t-np.pi)%(2*np.pi)
+    inc = inc_raw*np.pi/180
+    #R = np.array([[np.sin(inc)*np.cos(phi),   np.sin(phi),  np.cos(inc)*np.cos(phi)],
+    #              [-np.sin(inc)*np.sin(phi),  np.cos(phi),  -np.cos(inc)*np.sin(phi)],
+    #              [-np.cos(inc),              0,            np.sin(inc)]])
+    R = np.zeros((len(phi),3,3))
+    R[:,0,0] = np.sin(inc)*np.cos(phi)
+    R[:,0,1] = np.sin(phi)
+    R[:,0,2] = np.cos(inc)*np.cos(phi)
+    R[:,1,0] = -np.sin(inc)*np.sin(phi)
+    R[:,1,1] = np.cos(phi)
+    R[:,1,2] = -np.cos(inc)*np.sin(phi)
+    R[:,2,0] = -np.cos(inc)
+    R[:,2,2] = np.sin(inc)
+    a_mat = np.array([[1/r2**2,  0,        0],
+                      [0,        1/rp**2,  0],
+                      [0,        0,        1/rp**2]])[np.newaxis,:,:]
+    #[[a, d, f],
+    # [_, b, e],
+    # [_, _, c]] 
+    arr = np.matmul(R.transpose(0,2,1), np.matmul(a_mat,R))
+    a = arr[:,0,0]
+    b = arr[:,1,1]
+    c = arr[:,2,2]
+    d = arr[:,0,1]
+    e = arr[:,1,2]
+    f = arr[:,0,2]
+    return np.pi/np.sqrt(3*b*f**2/a + 3*c*d**2/a + -6*d*e*f/a + b*c - e**2)/(np.pi*rp**2)
+
+def phase_variation(time, t_sec, per, p0_phase, mode):
+    t = time - t_sec
+    w = 2*np.pi/per
+    phi = (w*t)
+    if 'v2' in mode:
+        A, B, C, D = p0_phase[:4]
+        phase = 1 + (A*(np.cos(phi)-1) + (B*np.sin(phi))) + C*(np.cos(2*phi)-1) + (D*np.sin(2*phi))
+    else:
+        A, B = p0_phase[:2]
+        phase = 1 + (A*(np.cos(phi)-1) + (B*np.sin(phi)))
+    return phase
+
+def fplanet_model(time, t0, per, rp, a, inc, ecc, w, u1, u2, fp, t_sec, p0_phase, mode):
+    phase = phase_variation(time, t_sec, per, p0_phase, mode)
+    eclip = eclipse(time, t0, per, rp, a, inc, ecc, w, u1, u2, fp, t_sec)
+    if 'ellipsoid' in mode:
+        sArea = area(time, t_sec, per, rp, inc, p0_phase, mode)
+    else:
+        sArea = 1
+    return sArea*phase*(eclip - 1)
+
+def ideal_lightcurve(time, p0, per, mode):
+    t0, rp, a, inc, ecosw, esinw, q1, q2, fp = p0[:9]
+    
+    ecc = np.sqrt(ecosw**2 + esinw**2)
+    w   = np.arctan2(esinw, ecosw)
+    u1  = 2*np.sqrt(q1)*q2
+    u2  = np.sqrt(q1)*(1-2*q2)
+    # create transit first and use orbital paramater to get time of superior conjunction
+    transit, t_sec, _ = transit_model(time, t0, per, rp, a, inc, ecc, w, u1, u2)
+    
+    #ugly way of doing this as might pick up detector parameters, but thats alright - faster this way and still safe
+    p0_phase = p0[9:13]
+    fplanet = fplanet_model(time, t0, per, rp, a, inc, ecc, w, u1, u2, fp, t_sec, p0_phase, mode)
+    
+    # add both light curves
+    f_total = transit + fplanet
+    return f_total
+
+def check_phase(time, p0, per, mode):
+    t0, rp, a, inc, ecosw, esinw, q1, q2, fp = p0[:9]
+    #ugly way of doing this as might pick up detector parameters, but thats alright - faster this way and still safe
+    p0_phase = p0[9:13]
+    
+    ecc = np.sqrt(ecosw**2 + esinw**2)
+    w   = np.arctan2(esinw, ecosw)
+    
+    params = batman.TransitParams()                      #object to store transit parameters
+    params.t0 = t0                                       #time of inferior conjunction
+    params.per = per                                     #orbital period
+    params.rp = rp                                       #planet radius (in units of stellar radii)
+    params.a = a                                         #semi-major axis (in units of stellar radii)
+    params.inc = inc                                     #orbital inclination (in degrees)
+    params.ecc = ecc                                     #eccentricity
+    params.w = w                                         #longitude of periastron (in degrees)
+    params.limb_dark = "uniform"                         #limb darkening model
+    params.u = []                                        #limb darkening coefficients
+    params.fp = fp                                       #planet/star brightnes
+    
+    m = batman.TransitModel(params, time)                #initializes model
+    t_sec = m.get_t_secondary(params)
+    params.t_secondary = t_sec
+    m = batman.TransitModel(params, time, transittype="secondary")  #initializes model
+    flux = m.light_curve(params)
+    
+    phase = phase_variation(time, t_sec, per, p0_phase, mode)
+    if 'ellipsoid' in mode:
+        sArea = area(time, t_sec, per, rp, inc, p0_phase, mode)
+    else:
+        sArea = 1
+    return np.any((sArea*phase*(flux-1)) < 0)
---
+++ helpers.py
@@ -0,0 +1,248 @@
+import numpy as np
+import scipy.optimize as spopt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib import gridspec
+
+import astro_models
+
+def get_data(path):
+    '''
+    Retrieve binned data
+    
+    Parameters
+    ----------
+    
+    path : string object
+        Full path to the data file output by photometry routine
+
+    Returns
+    -------
+
+    flux            : 1D array
+        Flux extracted for each frame
+
+    time             : 1D array
+        Time stamp for each frame
+
+    xdata            : 1D array
+        X-coordinate of the centroid for each frame
+
+    ydata            : 1D array
+        Y-coordinate of the centroid for each frame   
+
+    psfwx            : 1D array
+        X-width of the target's PSF for each frame     
+
+    psfwy            : 1D array
+    Y-width of the target's PSF for each frame     
+    '''
+    
+    #Loading Data
+    flux     = np.loadtxt(path, usecols=[0], skiprows=1)     # mJr/str
+    flux_err = np.loadtxt(path, usecols=[1], skiprows=1)     # mJr/str
+    time     = np.loadtxt(path, usecols=[2], skiprows=1)     # BMJD
+    xdata    = np.loadtxt(path, usecols=[4], skiprows=1)     # pixel
+    ydata    = np.loadtxt(path, usecols=[6], skiprows=1)     # pixel
+    psfxwdat = np.loadtxt(path, usecols=[8], skiprows=1)     # pixel
+    psfywdat = np.loadtxt(path, usecols=[10], skiprows=1)    # pixel
+    
+    factor = 1/(np.median(flux))
+    flux = factor*flux
+    flux_err = factor*flux
+    return flux, flux_err, time, xdata, ydata, psfxwdat, psfywdat
+
+def get_full_data(foldername, filename):
+    filename = filename[:13]+'full'+filename[19:]
+    path = foldername + filename
+    #Loading Data
+    flux     = np.loadtxt(path, usecols=[0], skiprows=1)     # mJr/str
+    flux_err = np.loadtxt(path, usecols=[1], skiprows=1)     # mJr/str
+    time     = np.loadtxt(path, usecols=[2], skiprows=1)     # hours
+    xdata    = np.loadtxt(path, usecols=[3], skiprows=1)     # hours
+    ydata    = np.loadtxt(path, usecols=[4], skiprows=1)     # hours
+    
+    return flux, flux_err, time, xdata, ydata
+
+def detec_model_poly(input_dat, c1, c2, c3, c4, c5, c6, c7=0, c8=0, c9=0, c10=0, c11=0, 
+                     c12=0, c13=0, c14=0, c15=0, c16=0, c17=0, c18=0, c19=0, c20=0, c21=0):
+    
+    xdata, ydata, mid_x, mid_y, mode = input_dat
+    x = xdata - mid_x
+    y = ydata - mid_y
+    
+    if 'Poly2' in mode:
+        pos = np.vstack((np.ones_like(x),x,y,x**2,x*y,y**2))
+        detec = np.array([c1, c2, c3, c4, c5, c6])
+    elif 'Poly3' in mode:
+        pos = np.vstack((np.ones_like(x),x,y,x**2,x*y,y**2,
+                        x**3,y*x**2,x*y**2,y**3))
+        detec = np.array([c1,  c2,  c3,  c4,  c5,  c6,
+                          c7,  c8,  c9,  c10,])
+    elif 'Poly4' in mode:
+        pos = np.vstack((np.ones_like(x),x,y,x**2,x*y,y**2,
+                        x**3,y*x**2,x*y**2,y**3,
+                        x**3*y,y**2*x**2,x*y**3,y**4))
+        detec = np.array([c1,  c2,  c3,  c4,  c5,  c6,
+                          c7,  c8,  c9,  c10,
+                          c11, c12, c13, c14, c15,])
+    elif 'Poly5' in mode:
+        pos = np.vstack((np.ones_like(x),x,y,x**2,x*y,y**2,
+                        x**3,y*x**2,x*y**2,y**3,
+                        x**3*y,y**2*x**2,x*y**3,y**4,
+                        x**5,x**4*y,x**3*y**2,x**2*y**3,x*y**4,y**5))
+        detec = np.array([c1,  c2,  c3,  c4,  c5,  c6,
+                          c7,  c8,  c9,  c10,
+                          c11, c12, c13, c14, c15,
+                          c16, c17, c18, c19, c20, c21])
+
+    return np.dot(detec[np.newaxis,:], pos).reshape(-1)
+
+def signal(time, xdata, ydata, mid_x, mid_y, per, p0, mode):
+    if 'Poly' in mode:
+        input_data = (xdata, ydata, mid_x, mid_y, mode)
+        start = 11
+        if 'ellipsoid' in mode:
+            start += 1
+        elif 'v2' in mode:
+            start += 2
+        p0_detec = p0[start:-1]
+        return astro_models.ideal_lightcurve(time, p0, per, mode)*detec_model_poly(input_data, *p0_detec)
+    else:
+        print('CANNOT DEAL WITH NON-POLY DETECTOR MAPS YET!!!')
+        return np.zeros_like(time)
+
+def lnprior(p0, initial, errs, time, per, mode):
+    t0, rp, a, inc, ecosw, esinw, q1, q2, fp = p0[:9]
+    t0_err, rp_err, a_err, inc_err = errs
+    check = astro_models.check_phase(time, p0, per, mode)
+    lgpri_t0 = -0.5*(((t0 - initial[0])/t0_err)**2.0)
+    lgpri_rp = 0 # -0.5*(((rp - initial[1])/(rp_err))**2.0)
+    lgpri_a = -0.5*(((a - initial[2])/a_err)**2.0)
+    lgpri_i = -0.5*(((inc - initial[3])/inc_err)**2.0)
+    # uniform prior for the rest
+    if ((0 < fp < 1) and (0 < q1 < 1) and (0 < q2 < 1) and 
+        (-1 < ecosw < 1) and (-1 < esinw < 1) and (check == False)):
+        return 0.0 + lgpri_t0 + lgpri_rp + lgpri_a + lgpri_i
+    else:
+        return -np.inf
+
+def lnprob(p0, time, flux, xdata, ydata, mid_x, mid_y, per, initial, errs, mode):
+    lp = lnprior(p0, initial, errs, time, per, mode)
+    if not np.isfinite(lp):
+        return -np.inf
+    model = signal(time, xdata, ydata, mid_x, mid_y, per, p0, mode)
+    flux_err = p0[-1]
+    lnlike = -0.5*np.sum((flux-model)**2)/(flux_err**2) - len(flux)*np.log(flux_err)
+    if not np.isfinite(lnlike):
+        return -np.inf
+    return lp + lnlike
+
+#Initial ML fit to detector parameters
+def fit_residual(flux, time, xdata, ydata, p0, u_detec, l_detec, per, mode):
+    # Use initial LC parameters
+    lcurve = astro_models.ideal_lightcurve(time, p0, per, mode)
+    # Get residuals
+    residuals = flux/lcurve
+    # Fit residual
+    coord = (xdata, ydata)
+    start = 11
+    if 'ellipsoid' in mode:
+        start += 1
+    elif 'v2' in mode:
+        start += 2
+    order = int(mode[mode.find('Poly')+4])
+    if order == 2:
+        func = det4fit_Poly2
+    elif order == 3:
+        func = det4fit_Poly3
+    elif order == 4:
+        func = det4fit_Poly4
+    elif order ==5:
+        func = det4fit_Poly5
+    p0_detec = p0[start:-1]
+    popt, pcov = spopt.curve_fit(func, coord, residuals, p0=p0_detec, bounds = (l_detec, u_detec))
+    return popt
+
+def walk_style(ndim, nwalk, samples, interv, subsamp, labels, fname=None):
+    '''
+    input:
+        ndim    = number of free parameters
+        nwalk   = number of walkers
+        samples = samples chain
+        interv  = take every 'interv' element to thin out the plot
+        subsamp = only show the last 'subsamp' steps
+    '''
+    # get first index
+    beg   = len(samples[0,:,0]) - subsamp
+    end   = len(samples[0,:,0]) 
+    step  = np.arange(beg,end)
+    step  = step[::interv] 
+    
+    # number of columns and rows of subplots
+    ncols = 4
+    nrows = int(np.ceil(ndim/ncols))
+    sizey = 2*nrows
+    
+    # plotting
+    plt.figure(figsize = (15, 2*nrows))
+    for ind in range(ndim):
+        plt.subplot(nrows, ncols, ind+1)
+        mu_param = np.mean(samples[:,:,ind][:,beg:end:interv], axis = 0)
+        std_param = np.std(samples[:,:,ind][:,beg:end:interv], axis = 0)
+        plt.plot(step, mu_param)
+        plt.fill_between(step, mu_param + 3*std_param, mu_param - 3*std_param, facecolor='k', alpha = 0.1)
+        plt.fill_between(step, mu_param + 2*std_param, mu_param - 2*std_param, facecolor='k', alpha = 0.1)
+        plt.fill_between(step, mu_param + 1*std_param, mu_param - 1*std_param, facecolor='k', alpha = 0.1)
+        plt.title(labels[ind])
+        plt.xlim(np.min(step), np.max(step))
+        if ind < (ndim - ncols):
+            plt.xticks([])
+        else: 
+            plt.xticks(rotation=25)
+    if fname != None:
+        plt.savefig(fname, bbox_inches='tight')
+    plt.show()
+    return    
+
+def chi2(data, fit, err):
+    return np.sum(((data - fit)/err)**2)
+
+def loglikelihood(data, fit, err):
+    return -0.5*chi2(data, fit, err) - np.sum(np.log(err)) #sum acts as N
+
+def BIC(logL, Npar, Ndat):
+    return logL - (Npar/2)*np.log(Ndat)
+
+def triangle_colors(data1, data2, data3, data4, label, path):
+    fig = plt.figure(figsize = (8,8))
+    gs  = gridspec.GridSpec(len(data1)-1,len(data1)-1)
+    i = 0
+    for k in range(np.sum(np.arange(len(data1)))):
+        j= k - np.sum(np.arange(i+1))
+        ax = fig.add_subplot(gs[i,j])
+        ax.plot(data1[j], data1[i+1],'k.', markersize = 0.2)
+        l1 = ax.plot(data2[j], data2[i+1],'.', color = '#66ccff', markersize = 0.7, label='$1^{st}$ secondary eclipse')
+        l2 = ax.plot(data3[j], data3[i+1],'.', color = '#ff9933', markersize = 0.7, label='transit')
+        l3 = ax.plot(data4[j], data4[i+1],'.', color = '#0066ff', markersize = 0.7, label='$2^{nd}$ secondary eclipse')
+        if (j == 0):
+            plt.setp(ax.get_yticklabels(), rotation = 45)
+            ax.yaxis.set_major_locator(MaxNLocator(5, prune = 'both'))
+            ax.set_ylabel(label[i+1])
+        else:
+            plt.setp(ax.get_yticklabels(), visible=False)
+        if (i == len(data1)-2):
+            plt.setp(ax.get_xticklabels(), rotation = 45)
+            plt.axhline(y=0, color='k', linestyle='dashed')
+            #ax.plot(bins[j], res[j], '.', color='#ff5050', markersize = 3)  # plot bin residual
+            ax.xaxis.set_major_locator(MaxNLocator(5, prune = 'both'))
+            ax.set_xlabel(label[j])
+        else:
+            plt.setp(ax.get_xticklabels(), visible=False)
+        if(i == j):
+            i += 1
+    handles = [l1,l2,l3]
+    fig.subplots_adjust(hspace=0)
+    fig.subplots_adjust(wspace=0)
+    fig.savefig(path, bbox_inches='tight')
+    return
