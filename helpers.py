a452953406ab03b1a6c678272bcfbcc4db4e7101
Lisa <kha.dang@mail.mcgill.ca>
2018-02-19 12:12:57 -0500

    minor change

--- helpers.py
+++ helpers.py
@@ -128,6 +128,8 @@ def time_sort_data(flux, flux_err, time, xdata, ydata, psfxw, psfyw, cut=0):
 	psfyw      = psfyw0[cut:]
 	return flux, flux_err, time, xdata, ydata, psfxw, psfyw
 
+
+
 def detec_model_poly(input_dat, c1, c2, c3, c4, c5, c6, c7=0, c8=0, c9=0, c10=0, c11=0, 
                      c12=0, c13=0, c14=0, c15=0, c16=0, c17=0, c18=0, c19=0, c20=0, c21=0):
     
