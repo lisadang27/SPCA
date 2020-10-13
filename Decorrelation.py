a5d2ed7ed1b8c7feb64c7a0974f2e06570e7a1f2
Taylor James Bell <taylor.bell@mail.mcgill.ca>
2020-10-13 17:03:36 -0400

    Fixing companion dilution correction bug.
    The variable planets was a list, so planets=='planetname' was just False, not a boolean array.

--- Decorrelation.py
+++ Decorrelation.py
@@ -22,8 +22,8 @@ from SPCA import helpers, astro_models, make_plots, make_plots_custom, detec_mod
 from SPCA import Decorrelation_helper as dh
 
 
-planets = ['CoRoT-2b', 'HAT-P-7b', 'KELT-1b', 'KELT-16b', 'KELT-9b', 'MASCARA-1b', 'Qatar-1b', 'WASP-103b', 'WASP-12b', 'WASP-12b_old', 'WASP-14b', 'WASP-18b', 'WASP-19b', 'WASP-33b', 'WASP-43b', 'HD189733b', 'HD209458b', 'HD149026b']
-channels = ['ch2' for planet in planets]
+planets = np.array(['CoRoT-2b', 'HAT-P-7b', 'KELT-1b', 'KELT-16b', 'KELT-9b', 'MASCARA-1b', 'Qatar-1b', 'WASP-103b', 'WASP-12b', 'WASP-12b_old', 'WASP-14b', 'WASP-18b', 'WASP-19b', 'WASP-33b', 'WASP-43b', 'HD189733b', 'HD209458b', 'HD149026b'])
+channels = np.array(['ch2' for planet in planets])
 
 rootpath = '/homes/picaro/bellt/research/'
 
